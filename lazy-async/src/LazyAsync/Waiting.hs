bc3833d19d0492c4509be3d4c9648e4996214860
Chris Martin <ch.martin@gmail.com>
2021-07-15 15:36:23 -0600

    doc wait and waitCatch

--- lazy-async/src/LazyAsync/Waiting.hs
+++ lazy-async/src/LazyAsync/Waiting.hs
@@ -15,20 +15,26 @@ import System.IO              (IO)
 waitCatchSTM :: LazyAsync a -> STM (Outcome a)
 waitCatchSTM = pollSTM >=> statusOutcomeSTM
 
+-- | Wait for the action to complete, and return its value.
+-- If the action throws an exception, then the exception is returned.
+-- Note that this function does __not__ start the action.
 waitCatch :: LazyAsync a -> IO (Outcome a)
 waitCatch = atomically . waitCatchSTM
 
+-- | Wait for the action to complete, and return its value.
+-- If the action throws an exception, then the exception is re-thrown.
+-- Note that this function does __not__ start the action.
 wait :: LazyAsync a -> IO a
 wait = waitCatch >=> outcomeSuccess
 
 -- | Begin running an asynchronous action, if it has not already begun.
 -- Then wait for it to complete, and return its value.
--- If the action threw an exception, then the exception is re-thrown.
+-- If the action throws an exception, then the exception is re-thrown.
 startWait :: LazyAsync a -> IO a
 startWait ao = start ao *> wait ao
 
 -- | Begin running an asynchronous action, if it has not already begun.
 -- Then wait for it to complete, and return its value.
--- If the action threw an exception, then the exception is returned.
+-- If the action throws an exception, then the exception is returned.
 startWaitCatch :: LazyAsync a -> IO (Outcome a)
 startWaitCatch ao = start ao *> waitCatch ao
