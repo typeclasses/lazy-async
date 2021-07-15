b17caddb93bb9aa38c04b39d7dba9d69f342c10e
Chris Martin <ch.martin@gmail.com>
2021-07-15 15:57:19 -0600

    remove transformers dependency

--- lazy-async/src/LazyAsync/Spawning.hs
+++ lazy-async/src/LazyAsync/Spawning.hs
@@ -4,19 +4,17 @@ import Control.Applicative         ((*>))
 import Control.Concurrent.Async    (withAsync)
 import Control.Concurrent.STM      (atomically, check)
 import Control.Concurrent.STM.TVar (TVar, newTVarIO, readTVar)
-import Control.Monad               (return, (>>=))
-import Control.Monad.Trans.Class   (lift)
-import Control.Monad.Trans.Cont    (ContT (ContT, runContT))
+import Control.Monad               ((>>=))
 import Data.Bool                   (Bool (False))
 import Data.Function               (($))
 import LazyAsync.LazyAsync         (LazyAsync (A1))
 import System.IO                   (IO)
 
 withLazyAsync :: IO a -> (LazyAsync a -> IO b) -> IO b
-withLazyAsync action = runContT $ do
-    s <- lift $ newTVarIO False
-    a <- ContT $ withAsync $ waitForTrueIO s *> action
-    return $ A1 s a
+withLazyAsync action continue =
+    newTVarIO False >>= \s ->
+    withAsync (waitForTrueIO s *> action) $ \a ->
+    continue $ A1 s a
 
 waitForTrueIO :: TVar Bool -> IO ()
 waitForTrueIO x = atomically $ readTVar x >>= check
