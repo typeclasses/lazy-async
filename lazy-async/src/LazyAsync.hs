78fb9acb23ee0f0d88f4a2d27424a0c2c8d98685
Chris Martin <ch.martin@gmail.com>
2021-07-16 15:12:51 -0600

    doc about applicative instances

--- lazy-async/src/LazyAsync.hs
+++ lazy-async/src/LazyAsync.hs
@@ -1,13 +1,14 @@
 module LazyAsync
-  ( {- * Asynchronous actions -} LazyAsync,
-    {- * Spawning -}             withLazyAsync,
-    {- * Getting results -}      startWait,
-    {- * Catching exceptions -}  startWaitCatch, Outcome (..),
-    {- * Polling -}              poll, Status (..),
-    {- * Starting manually -}    start, wait, waitCatch,
-    {- * Transactions -}         pollSTM, startSTM, waitCatchSTM,
-    {- * IO specializations -}   withLazyAsyncIO, startWaitIO, startWaitCatchIO,
-                                 startIO, pollIO, waitIO, waitCatchIO
+  ( {- * Asynchronous actions -}  LazyAsync,
+    {- * Spawning -}              withLazyAsync,
+    {- * Getting results -}       startWait,
+    {- * Applicative instances -} {- $Applicative -}
+    {- * Catching exceptions -}   startWaitCatch, Outcome (..),
+    {- * Polling -}               poll, Status (..),
+    {- * Starting manually -}     start, wait, waitCatch,
+    {- * Transactions -}          pollSTM, startSTM, waitCatchSTM,
+    {- * IO specializations -}    withLazyAsyncIO, startWaitIO, startWaitCatchIO,
+                                  startIO, pollIO, waitIO, waitCatchIO
   ) where
 
 import LazyAsync.LazyAsync (LazyAsync)
@@ -19,3 +20,44 @@ import LazyAsync.Status    (Status (..))
 import LazyAsync.Waiting   (startWait, startWaitCatch, startWaitCatchIO,
                             startWaitIO, wait, waitCatch, waitCatchIO,
                             waitCatchSTM, waitIO)
+
+{- $Applicative
+
+'LazyAsync' has an 'Control.Applicative.Applicative' instance that lets us build
+up complexes that combine results from multiple asynchronous actions.
+
+* When we 'start' a complex, it starts all of its parts.
+
+* When we 'wait' on a complex, it returns a 'Success' result once all of its
+parts complete successfully.
+
+* When any part resolves to a 'Failure' outcome, the complex fails and 'wait'
+throws an exception immediately.
+
+* When we 'wait' on a complex that suffers failures in multiple parts, the
+exception thrown is the one that comes from the part that failed first
+chronologically.
+
+* When one part of a complex fails, the other parts remain running. This is
+because their results may still be wanted elsewhere.
+
+When we 'poll' a complex that has suffered failures in multiple parts, we see
+the leftmost 'Failure'. This can have potentially surprising consequences:
+
+* Whe exception we see when we 'poll' a failed 'LazyAsync' might not be the same
+exception that was thrown by 'wait'.
+
+* 'poll'ing a failed 'LazyAsync' is not guaranteed to give the same exception
+each time.
+
+The 'Control.Applicative.Applicative' instances for 'Outcome' and 'Status' are
+chosen to produce the fail-fast behavior of 'LazyAsync'.
+
+* 'Outcome' behaves the same as 'Data.Either.Either', halting at the leftmost
+'Failure'.
+
+* If any part of a complex is 'Failure', then the complex evaluates to
+'Failure', even if some parts are 'Incomplete'. For example,
+@'Incomplete' 'Control.Applicative.<*>' 'Failure' e@ = 'Failure' e.
+
+-}
