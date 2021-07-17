a88a36b35be63dc0674ba5e80b014fab633a514f
Chris Martin <ch.martin@gmail.com>
2021-07-16 18:09:05 -0600

    wibble

--- lazy-async/src/LazyAsync.hs
+++ lazy-async/src/LazyAsync.hs
@@ -2,14 +2,14 @@
 
 module LazyAsync
   ( {- * Asynchronous actions -}  LazyAsync,
-    {- * Spawning -}              withLazyAsync,
-    {- * Getting results -}       startWait,
-    {- * Combining actions -}     apply,
-    {- * Catching exceptions -}   startWaitCatch, Outcome (..), applyOutcome,
-    {- * Polling -}               poll, Status (..), applyStatus,
-    {- * Starting manually -}     start, wait, waitCatch,
-    {- * Transactions -}          pollSTM, startSTM, waitCatchSTM,
-    {- * IO specializations -}    withLazyAsyncIO, startWaitIO, startWaitCatchIO,
+    {- * Spawning             -}  withLazyAsync,
+    {- * Getting results      -}  startWait,
+    {- * Combining actions    -}  apply,
+    {- * Catching exceptions  -}  startWaitCatch, Outcome (..), applyOutcome,
+    {- * Polling              -}  poll, Status (..), applyStatus,
+    {- * Starting manually    -}  start, wait, waitCatch,
+    {- * Transactions         -}  pollSTM, startSTM, waitCatchSTM,
+    {- * IO specializations   -}  withLazyAsyncIO, startWaitIO, startWaitCatchIO,
                                   startIO, pollIO, waitIO, waitCatchIO
   ) where
 
